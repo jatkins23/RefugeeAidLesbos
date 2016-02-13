ace5516afbd4379291f41075ddfc01d9b48eebd5
Owen Martin <martin.owen22@gmail.com>
2016-02-13 08:39:30 -0500

    Trying to make the map work

--- lesbosapp/app/controllers/resources_controller.rb
+++ lesbosapp/app/controllers/resources_controller.rb
@@ -2,4 +2,9 @@ class ResourcesController < ApplicationController
   def index
     @resources = Resource.all
   end
+  def show
+  	@resource = Resource.find(params[:id])
+  	render :json @resource
+  end
 end
+
