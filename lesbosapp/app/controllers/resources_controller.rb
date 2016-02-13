8765dc5ff7bb0789d1a3c435591c1580bfec9aa3
Jon Atkins <jon.atkins23@gmail.com>
2016-02-13 10:02:46 -0500

    removed merge conflict

--- lesbosapp/app/controllers/resources_controller.rb
+++ lesbosapp/app/controllers/resources_controller.rb
@@ -2,8 +2,7 @@ class ResourcesController < ApplicationController
   def index
     @resources = Resource.all
   end
-<<<<<<< HEAD
-
+  
   def show
     @resource = Resource.find(params[:id])
     render :json => @resource
