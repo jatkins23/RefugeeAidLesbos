cc797bbb9979630868ee7b46b64e34a93303d6d3
rickypchen <richard.polin.chen@gmail.com>
2016-02-16 19:40:34 -0500

    added pins based on geo lat lng

--- lesbosapp/app/controllers/resources_controller.rb
+++ lesbosapp/app/controllers/resources_controller.rb
@@ -1,11 +1,30 @@
 class ResourcesController < ApplicationController
   def index
     @resources = Resource.all
+
+    @hash = Gmaps4rails.build_markers(resources_with_latlng) do |resource, marker|
+        marker.lat resource.lat
+        marker.lng resource.lng
+        marker.title resource.name
+        marker.infowindow resource.name
+    end
+
   end
-  
+
   def show
     @resource = Resource.find(params[:id])
     render :json => @resource
   end
-end
 
+  private
+  def resources_with_latlng
+    @resources_with_latlng = []
+
+    Resource.all.each do |resource|
+      if resource.lat != nil && resource.lng != nil
+            @resources_with_latlng << resource
+      end
+    end
+  @resources_with_latlng
+  end
+end
--- lesbosapp/app/models/resource.rb
+++ lesbosapp/app/models/resource.rb
@@ -1,2 +1,4 @@
 class Resource < ActiveRecord::Base
+  geocoded_by :location
+  after_validation :geocode
 end
